fd83c1b72fb8456a5ce8aa8050fe7be192c50693
Maurício Caminha <mauricio.caminha@hotmail.com>
2025-01-26 15:29:08 -0300

    adiciona interfaces e métodos para busca de organizações próximas

--- src/@types/orgs-repository.ts
+++ src/@types/orgs-repository.ts
@@ -1,6 +1,13 @@
 import { Prisma, Org } from "@prisma/client";
 
+export interface FindManyNearby {
+  latitude: number;
+  longitude: number;
+}
+
 export interface OrgsRepository {
   create(data: Prisma.OrgCreateInput): Promise<Org>;
   findByEmail(email: string): Promise<Org | null>;
+  findById(id: string): Promise<Org | null>;
+  findManyNearby(params: FindManyNearby): Promise<Org[]>;
 }
--- src/repositories/in-memory/in-memory-orgs-repository.ts
+++ src/repositories/in-memory/in-memory-orgs-repository.ts
@@ -1,7 +1,8 @@
 import { Prisma, Org } from "@prisma/client";
 import { randomUUID } from "node:crypto";
 
-import { OrgsRepository } from "@/@types/orgs-repository";
+import { FindManyNearby, OrgsRepository } from "@/@types/orgs-repository";
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
 
 export class InMemoryOrgsRepository implements OrgsRepository {
   public items: Org[] = [];
@@ -20,5 +21,23 @@ export class InMemoryOrgsRepository implements OrgsRepository {
 
   async findByEmail(email: string) {
     return this.items.find((org) => org.email === email) || null;
+  }
+
+  async findById(id: string) {
+    return this.items.find((org) => org.id === id) || null;
+  }
+
+  async findManyNearby(params: FindManyNearby) {
+    return this.items.filter((item) => {
+      const distance = getDistanceBetweenCoordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        }
+      );
+
+      return distance < 10;
+    });
   }
 }
